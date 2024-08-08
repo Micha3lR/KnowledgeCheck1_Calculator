471378c9587fbca3b9b28af87f8d20e728aaa2d8
Micha3lR <mrevans91@hotmail.com>
2024-08-08 00:51:53 -0400

    New Calculator Project for Knowledge Check 1

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -19,62 +19,69 @@ namespace KnowledgeCheck1_Calculator
             switch (input)
             {
                 case "1":
-                    Console.WriteLine("Enter 2 integers to add");
-                    var addNumber1 = Console.ReadLine();
-                    var addNumber2 = Console.ReadLine();
+                    int addNumOne, addNumTwo;
 
-                    if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
+                    if (TryGetIntegers(out addNumOne, out addNumTwo))
                     {
-                        Console.Write($"{addNumber1} + {addNumber2} = ");
+                        Console.Write($"{addNumOne} + {addNumTwo} = ");
                         Console.Write(calculator.Add(addNumOne, addNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
                     }
                     break;
 
                 case "2":
-                    Console.WriteLine("Enter 2 integers to subtract");
-                    var subtractNumber1 = Console.ReadLine();
-                    var subtractNumber2 = Console.ReadLine();
+                    int subNumOne, subNumTwo;
 
-                    if (int.TryParse(subtractNumber1, out int subNumOne) && int.TryParse(subtractNumber2, out int subNumTwo))
+                    if (TryGetIntegers(out subNumOne, out subNumTwo))
                     {
-                        Console.Write($"{subtractNumber1} - {subtractNumber2} = ");
+                        Console.Write($"{subNumOne} - {subNumTwo} = ");
                         Console.Write(calculator.Subtract(subNumOne, subNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
                     }
                     break;
 
                 case "3":
-                    // Add code here
+                    int multNumOne, multNumTwo;
+
+                    if (TryGetIntegers(out multNumOne, out multNumTwo))
+                    {
+                        Console.Write($"{multNumOne} * {multNumTwo} = ");
+                        Console.Write(calculator.Multiply(multNumOne, multNumTwo));
+                    }
                     break;
 
                 case "4":
-                    Console.WriteLine("Enter 2 integers to divide");
-                    var divideNumber1 = Console.ReadLine();
-                    var divideNumber2 = Console.ReadLine();
+                    double divNumOne, divNumTwo;
 
-                    if (double.TryParse(divideNumber1, out double divNumOne) && double.TryParse(divideNumber2, out double divNumTwo))
+                    if (TryGetDoubles(out divNumOne, out divNumTwo))
                     {
-                        Console.Write($"{divideNumber1} / {divideNumber2} = ");
+                        Console.Write($"{divNumOne} / {divNumTwo} = ");
                         Console.Write(calculator.Divide(divNumOne, divNumTwo));
                     }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
-                    break;
                     break;
 
                 default:
                     Console.WriteLine("Unknown input");
                     break;
             }
+
+        }
+        static bool TryGetIntegers (out int numOne, out int numTwo)
+        {
+            Console.WriteLine("Enter two numbers:");
+            string inputOne = Console.ReadLine();
+            string inputTwo = Console.ReadLine();
+            numOne = 0;
+            numTwo = 0;
+            return int.TryParse(inputOne, out numOne) && int.TryParse(inputTwo, out numTwo);
+        }
+
+        static bool TryGetDoubles (out double numOne, out double numTwo)
+        {
+            Console.WriteLine("Enter two numbers:");
+            string inputOne = Console.ReadLine();
+            string inputTwo = Console.ReadLine();
+            numOne = 0.0;
+            numTwo = 0.0;
+            return double.TryParse(inputOne, out numOne) && double.TryParse(inputTwo,out numTwo);
         }
     }
 }
